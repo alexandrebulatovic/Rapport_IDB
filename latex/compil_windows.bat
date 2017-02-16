801dc8b55aa1f447c134230339ccc99cba4f02ca
golngaz <golngaz@laposte.net>
2017-02-16 17:59:55 +0100

    [tests] gloasssaire++

--- latex/compil_windows.bat
+++ latex/compil_windows.bat
@@ -3,9 +3,18 @@ SET BINDIR=%~dp0
 CD /D "%BINDIR%"
 
 pdflatex main.tex
+makeglossaries main
+pdflatex main.tex
+pdflatex main.tex
+
+
 rename main.pdf IDB.pdf
 move /y IDB.pdf compil
 move /y main.tex compil
 del main.*
 cd compil
 move /y main.tex ../
+
+cls
+IDB.pdf
+cd ..
